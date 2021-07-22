96d4eda2a48d13ac6a60a88b0712474679cf18a1
Lusi01 <rajnisk@hotmail.com>
2021-07-22 19:35:22 +0400

    The sixth control work_3

--- realworlddjango/settings.py
+++ realworlddjango/settings.py
@@ -41,12 +41,22 @@ LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = reverse_lazy('accounts:sign_in')
 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+
+EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_PORT = 2525
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+
 
 #
 # SITE_ID = 1
